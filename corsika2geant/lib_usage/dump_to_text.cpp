5b547fc6b7306492682da50856ae08e954cba01f
Igor Vaiman <gosha.vaiman@gmail.com>
2021-02-13 22:20:28 +0300

    dump to text tool updated

--- corsika2geant/lib_usage/dump_to_text.cpp
+++ corsika2geant/lib_usage/dump_to_text.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <iostream>
+#include <fstream>
 
 #include "../corsika2geant.hpp"
 
@@ -7,19 +8,26 @@ using namespace std;
 
 
 int main(int argc, char **argv) {
-  if (argc<2) {
-    cout << "specify CORSIKA DAT file path" << endl;
+  if (argc<3) {
+    cout << "specify CORSIKA DAT file path and outdir" << endl;
     return 1;
   }
   
   string corsika_file_path = argv[1];
+  string outdir = argv[2];
+
   vector<ShowerData> sds = LoadShowerDataFromFile(corsika_file_path);
+
+  ofstream output_file;
+  int last_slash_position = corsika_file_path.find_last_of("/");
+  string corsika_file_name = corsika_file_path.substr(last_slash_position + 1, corsika_file_path.length() - last_slash_position);
+  output_file.open(outdir + "/" + corsika_file_name + ".tsv");
 
   ShowerData sd = sds[0];
 
-  cout << "id\tx\ty\tpx\tpy\tpz\tE\tt" << endl;
+  output_file << "id\tx\ty\tpx\tpy\tpz\tE\tt" << endl;
   for(vector<ParticleData>::iterator particle = sd.particles.begin(); particle != sd.particles.end(); ++particle) {
-    cout <<
+    output_file <<
       particle->id << "\t" <<
       particle->x << "\t" <<
       particle->y << "\t" <<
@@ -30,6 +38,8 @@ int main(int argc, char **argv) {
       particle->t << "\t" <<
       endl;
   }
+
+  output_file.close();
 
   return 0;
 }
